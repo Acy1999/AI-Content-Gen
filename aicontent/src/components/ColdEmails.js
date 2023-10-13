cd90a15c45153f80a551cc8ac3732cf1f6e5fc7b
Tony Young <acy1999@gmail.com>
2023-10-13 16:14:58 -0700

    New commit

--- aicontent/src/components/ColdEmails.js
+++ aicontent/src/components/ColdEmails.js
@@ -1,43 +1,34 @@
-import React from 'react'
-import {
-  Component
-} from 'react'
-import {
-  Container,
-  Form,
-  Button,
-  Card
-} from 'react-bootstrap'
+import React from "react";
+import { Component } from "react";
+import { Container, Form, Button, Card } from "react-bootstrap";
 
-const {
-  Configuration,
-  OpenAIApi
-} = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 
 class ColdEmails extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      heading: 'The Response from the AI will be shown here',
-      response: '... await the response'
-    }
+      heading: "The Response from the AI will be shown here",
+      response: "... await the response",
+    };
   }
 
-  onFormSubmit = e => {
+  onFormSubmit = (e) => {
     //start by preventing the default
-    e.preventDefault()
+    e.preventDefault();
 
     const formData = new FormData(e.target),
-      formDataObj = Object.fromEntries(formData.entries())
-    console.log(formDataObj.emailDescriptor)
-    console.log(process.env.REACT_APP_OPENAI_API_KEY)
+      formDataObj = Object.fromEntries(formData.entries());
+    console.log(formDataObj.emailDescriptor);
+    console.log(process.env.REACT_APP_OPENAI_API_KEY);
     //////OPENAI
     const configuration = new Configuration({
       apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
 
-    openai.createCompletion("text-davinci-002", {
+    openai
+      .createCompletion("text-davinci-002", {
         prompt: `Write a professional email about ${formDataObj.emailDescriptor}.`,
         temperature: 0.9,
         max_tokens: 200,
@@ -48,96 +39,64 @@ class ColdEmails extends Component {
       .then((response) => {
         this.setState({
           heading: `Cold email suggestion: ${formDataObj.emailDescriptor}`,
-          response: `${response.data.choices[0].text}`
-        })
+          response: `${response.data.choices[0].text}`,
+        });
       });
-  }
+  };
 
   render() {
-
-    return ( <
-      div >
-      <
-      Container >
-      <
-      br / >
-      <
-      br / >
-      <
-      h1 > Generate Emails < /h1> <
-      br / >
-      <
-      h4 > Generate Emails
-      for anything.All you need to do is enter the content you want to talk about to get sarted. < /h4> <
-        br / >
-        <
-        br / >
-        <
-        Form onSubmit = {
-          this.onFormSubmit
-        } >
-        <
-        Form.Group className = "mb-3"
-      controlId = "formBasicEmail" >
-      <
-      Form.Label > What would you like us to email about ? < /Form.Label> <
-      Form.Control type = "text"
-      name = "emailDescriptor"
-      placeholder = "Enter what your email is about." / >
-      <
-      Form.Text className = "text-muted" >
-      Enter as much information as possible
-      for more accurate Emails. <
-      /Form.Text> <
-      /Form.Group>
-
-      <
-      Button variant = "primary"
-      size = "lg"
-      type = "submit" >
-      Get AI Suggestions <
-      /Button> <
-      /Form>
-
-      <
-      br / >
-      <
-      br / >
-
-      <
-      Card >
-      <
-      Card.Body >
-      <
-      Card.Title > < h1 > {
-        this.state.heading
-      } < /h1></Card.Title >
-      <
-      br / >
-      <
-      br / >
-      <
-      Card.Text >
-      <
-      h4 > {
-        this.state.response
-      } <
-      /h4> <
-      /Card.Text> <
-      /Card.Body> <
-      /Card> <
-      /Container> <
-      br / >
-      <
-      br / >
-      <
-      br / >
-      <
-      br / >
-      <
-      /div>
-    )
+    return (
+      <div>
+        <Container>
+          <br />
+          <br />
+          <h1> Generate Emails </h1>
+          <br />
+          <h4>
+            {" "}
+            Generate Emails for anything. All you need to do is enter the
+            content you want to talk about to get sarted.{" "}
+          </h4>{" "}
+          <br />
+          <br />
+          <Form onSubmit={this.onFormSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label> What would you like us to email about ? </Form.Label>{" "}
+              <Form.Control
+                type="text"
+                name="emailDescriptor"
+                placeholder="Enter what your email is about."
+              />
+              <Form.Text className="text-muted">
+                Enter as much information as possible for more accurate Emails.{" "}
+              </Form.Text>{" "}
+            </Form.Group>
+            <Button variant="primary" size="lg" type="submit">
+              Get AI Suggestions{" "}
+            </Button>{" "}
+          </Form>
+          <br />
+          <br />
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                {" "}
+                <h1> {this.state.heading} </h1>
+              </Card.Title>
+              <br />
+              <br />
+              <Card.Text>
+                <h4> {this.state.response} </h4>{" "}
+              </Card.Text>{" "}
+            </Card.Body>{" "}
+          </Card>
+        </Container>
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    );
   }
 }
-
-export default ColdEmails
+export default ColdEmails;
